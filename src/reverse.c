165fe60b42f12e5a0b22e4ca2f8496bee13eaa3c
LE Dylan <dylan.le@ecole-89.com>
2022-12-08 16:30:51 +0100

    reverse

---
+++ src/reverse.c
@@ -0,0 +1,32 @@
+#include "objectif.h"
+#include <stdio.h>
+
+char *rev(char *buffer)
+{
+    int i;
+    int j;
+    char *k;
+    int a;
+    a = 0;
+    i = 0;
+    j = 0;
+    k = malloc(sizeof(char) * 32);
+    while(buffer[j] != '\0') {
+        while (buffer[i] != '\n'){
+            i += 1;
+        }
+        k[i] = '\n';
+        a = i + 1;
+        while(buffer[j] != '\n'){
+            k[j] = buffer[i - 1];
+            j += 1;
+            i -= 1;
+        }
+        i = a;
+        j += 1;
+    }
+    write(1, k, stu_strlen(k));
+    // write(1,"\n",1);
+    free(k);
+    return(0);
+}
