b58dcf0733277737d5a0dd0da496193058055944
LE Dylan <dylan.le@ecole-89.com>
2022-12-13 09:33:51 +0100

    Include

---
+++ Include/objectif.h
@@ -0,0 +1,12 @@
+#ifndef OBJECTIF_H_
+#define OBJECTIF_H_
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+unsigned int stu_strlen(const char *str);
+char *rev(char *buffer);
+int *reverses(char *buffer);
+char *stu_strcat(char *a, const char *b);
+char *strdup(const char *s);
+char *stu_strcpy(char *dest, const char *src);
+#endif /*OBJECTIF_H_*/
