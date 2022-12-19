01912f92e2af639775459c00662dd0cf0b16acc9
LE Dylan <dylan.le@ecole-89.com>
2022-12-19 17:01:17 +0100

    update

--- src/stu_strlen.c
+++ src/stu_strlen.c
@@ -1,9 +1,18 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     strlen
+ * created on:  2022-12-19 - 16:41 +0100
+ * 1st author:  dylan.le - dylan.le
+ * description: rev
+ */
+
 unsigned int stu_strlen(const char *str)
 {
-    int i = 0;
+    int i;
 
+    i = 0;
     while (str[i] != ('\0'))
-        i +=1;
+        i += 1;
     {
         return (i);
     }
