4d41299630e8df9262b832611713a145df92630f
LE Dylan <dylan.le@ecole-89.com>
2022-12-20 10:17:32 +0100

    update

--- src/strdup.c
+++
@@ -1,9 +0,0 @@
-#include "objectif.h"
-
-char *strdup(const char * s)
-{
-    char * m;
-    m = malloc(sizeof(char) *stu_strlen(s));
-    stu_strcpy(m, s);
-    return (m);
-}
--- src/strlen.c
+++
@@ -1,8 +0,0 @@
-unsigned int stu_strlen(const char *str){
-    int i;
-    i = 0;
-    while(str[i] != '\0'){
-        i += 1;
-    }
-    return(i);
-}
